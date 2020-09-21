30dd9b29c3f9b504f5f5f48e39ad78e9c1a95f80
Vuchula <70232007+Vuchula@users.noreply.github.com>
2020-09-20 21:42:55 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -62,8 +62,6 @@ namespace Assignment1_Fall20
                 int sum = 0;
                 for (int i = 1; i <= n; i++)
 				{
-
-					Console.Write("{0} ", (2 * i) - 1);
                     sum += 2*i-1;
                     
                 }
@@ -148,21 +146,59 @@ namespace Assignment1_Fall20
         {
             try
             {
-                
+                int time=0,count=0,prev=0;
+                for (int i=0;i<keyboard.Length;i++)
+				{
+                    for(int j=0;j<word.Length;j++)
+					{
+                        if (keyboard[i].Equals(word[j]))
+                        {
+                            count = Math.Abs(i - prev);
+                            time += count;
+                            prev = i;
+                        }                        
+					}                    
+                }
+                Console.WriteLine("Time Elapsed" + time);
             }
             catch
             {
                 Console.WriteLine("Exception occured while computing BullsKeyboard()");
-            }
- 
-            return 0; 
+			}
+            return 0;
         }
  
         public static int StringEdit(string str1, string str2)
         {
             try
             {
-                // Write your code here
+                var dp = new int[str1.Length + 1][];
+                for (int i = 0; i < dp.Length; i++)
+                {
+                    dp[i] = new int[str2.Length + 1];
+                    dp[i][0] = i;
+                }
+
+                for (int j = 0; j < dp[0].Length; j++)
+                {
+                    dp[0][j] = j;
+                }
+
+                for (int i = 1; i < dp.Length; i++)
+                {
+                    for (int j = 1; j < dp[0].Length; j++)
+                    {
+                        if (str1[i - 1] == str2[j - 1])
+                        {
+                            dp[i][j] = dp[i - 1][j - 1];
+                        }
+                        else
+                        {
+                            dp[i][j] = Math.Min(Math.Min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                        }
+                    }
+                }
+                return dp[str1.Length][str2.Length];
             }
             catch
             {
